1ced758f2f62959f6ea84c90f7e57c6cb642215b
Aditya Bansod <hyperionab@yahoo.com>
2015-01-07 22:33:51 -0800

    Initial Commit

---
+++ TextSomeone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextSomeone
+//
+//  Created by Aditya Bansod on 1/7/15.
+//  Copyright (c) 2015 Aditya Bansod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
